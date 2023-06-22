c8364c4bc06a21b65046f594a14d99f9f3662bba
lTomaas <psilva@fi.uba.ar>
2023-06-22 11:57:08 -0300

    test

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -6,7 +6,7 @@ const DB_HOST = process.env.DB_HOST || '127.0.0.1'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'faztpassword'
 const DB_NAME = process.env.DB_NAME || 'usersdb'
-const DB_PORT = process.env.DB_PORT || '3306'
+const DB_PORT = process.env.DB_PORT || 3306
 
 
 const pool = new Pool({
