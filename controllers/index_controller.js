c297a88f54a78376424b355973095580d98004c0
lTomaas <psilva@fi.uba.ar>
2023-06-22 11:54:45 -0300

    test

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -2,11 +2,11 @@
 const { Pool } = require("pg");
 
 
-const DB_HOST = process.env.DB_HOST
+const DB_HOST = process.env.DB_HOST || '127.0.0.1'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'faztpassword'
 const DB_NAME = process.env.DB_NAME || 'usersdb'
-const DB_PORT = process.env.DB_PORT || 3306
+const DB_PORT = process.env.DB_PORT || '3306'
 
 
 const pool = new Pool({
